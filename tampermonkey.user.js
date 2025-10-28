13bcfc5bf1b994ed35551630899d2497b68bcfea
Huy Vũ <quanghuy25121995@gmail.com>
2025-10-29 00:46:45 +0700

    Update tampermonkey.user.js

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -4,9 +4,9 @@
 // @version      1.0
 // @description  Hiển thị UI trên mọi trang, tự động điều hướng về celebrity.html khi chạy logic.
 // @author       Huy Vũ
-// @require      https://YOUR_SERVER_URL/locket-auto-celeb.main.js
+// @require      https://raw.githubusercontent.com/huyvu2512/locket-auto-celeb.main.js
 // @match        https://locket.binhake.dev/*
 // @run-at       document-idle
 // @grant        none
-// @icon         https://github.com/user-attachments/assets/1ab52ab7-ecac-4f96-9603-7a7a7d0fb167
+// @icon         https://github.com/user-attachments/assets/a5738cf1-763a-4a4b-bf07-c9d1d335910a
 // ==/UserScript==
