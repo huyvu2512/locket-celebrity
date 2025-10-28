82c464557a516eb2503d7f09d6b3d2ff92ebb7a7
Huy Vũ <quanghuy25121995@gmail.com>
2025-10-29 00:45:12 +0700

    Create tampermonkey.user.js

---
+++ tampermonkey.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Auto Locket Celeb (v1.0)
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Hiển thị UI trên mọi trang, tự động điều hướng về celebrity.html khi chạy logic.
+// @author       Huy Vũ
+// @require      https://YOUR_SERVER_URL/locket-auto-celeb.main.js
+// @match        https://locket.binhake.dev/*
+// @run-at       document-idle
+// @grant        none
+// @icon         https://github.com/user-attachments/assets/1ab52ab7-ecac-4f96-9603-7a7a7d0fb167
+// ==/UserScript==
