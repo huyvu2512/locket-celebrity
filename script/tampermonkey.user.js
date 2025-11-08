3cfa217864d20cb6c93e8c69803455b44a13ae3c
Huy Vũ <quanghuy25121995@gmail.com>
2025-11-08 23:39:03 +0700

    Update tampermonkey.user.js

--- script/tampermonkey.user.js
+++ script/tampermonkey.user.js
@@ -4,7 +4,7 @@
 // @version      1.2
 // @description  Tự động kết bạn với tất cả Celeb, hẹn giờ tùy chỉnh để khởi động lại web.
 // @author       Huy Vũ
-// @require      https://raw.githubusercontent.com/huyvu2512/locket-celebrity/main/script/locket-auto-celeb.main.js
+// @require      https://raw.githubusercontent.com/huyvu2512/locket-celebrity/main/script/locket-auto-celeb.main.js.main.js
 // @homepageURL  https://github.com/huyvu2512/locket-celebrity/tree/main/script
 // @downloadURL  https://raw.githubusercontent.com/huyvu2512/locket-celebrity/main/script/tampermonkey.user.js
 // @updateURL    https://raw.githubusercontent.com/huyvu2512/locket-celebrity/main/script/tampermonkey.user.js
@@ -13,4 +13,5 @@
 // @grant        GM_xmlhttpRequest
 // @connect      open.oapi.vn
 // @icon         https://i.imgur.com/AM2f24N.png
+
 // ==/UserScript==
