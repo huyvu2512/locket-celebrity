6932d9b3eebbe61757a6fb74fc897c44126824a3
Huy Vũ <quanghuy25121995@gmail.com>
2025-11-26 12:02:59 +0700

    Update tampermonkey.user.js

--- script/src/tampermonkey.user.js
+++ script/src/tampermonkey.user.js
@@ -1,26 +1,20 @@
 // ==UserScript==
-// @name         Auto Locket Celeb (v1.9 UI fix)
+// @name         Auto Locket Celeb (v1.3)
 // @namespace    http://tampermonkey.net/
-// @version      1.9
-// @description  Sửa lỗi UI, di chuyển nút toggle sang phải và tăng kích thước popup.
+// @version      1.3
+// @description  Tự động kết bạn với tất cả Celeb, hẹn giờ tùy chỉnh để khởi động lại web.
 // @author       Huy Vũ
-// @match        https://locket.binhake.dev/*
-// @run-at       document-idle
-// @grant        GM_xmlhttpRequest
-// @connect      open.oapi.vn
-// @icon         //i.imgur.com/AM2f24N.png
-
-// === BỔ SUNG ĐỂ TẢI CÁC FILE PHỤ ===
-// Đảm bảo các URL này là đường dẫn RAW (thô) đến các file
 // @require      https://raw.githubusercontent.com/huyvu2512/locket-celebrity/main/script/src/config.js
 // @require      https://raw.githubusercontent.com/huyvu2512/locket-celebrity/main/script/src/styles.js
 // @require      https://raw.githubusercontent.com/huyvu2512/locket-celebrity/main/script/src/utils.js
 // @require      https://raw.githubusercontent.com/huyvu2512/locket-celebrity/main/script/src/ui.js
 // @require      https://raw.githubusercontent.com/huyvu2512/locket-celebrity/main/script/src/logic.js
-// @require      https://raw.githubusercontent.com/huyvu2512/locket-celebrity/main/script/src/timer.js
-// ===================================
-
+// @homepageURL  https://github.com/huyvu2512/locket-celebrity/tree/main/script
+// @downloadURL  https://raw.githubusercontent.com/huyvu2512/locket-celebrity/main/script/tampermonkey.user.js
+// @updateURL    https://raw.githubusercontent.com/huyvu2512/locket-celebrity/main/script/tampermonkey.user.js
+// @match        https://locket.binhake.dev/*
+// @run-at       document-idle
+// @grant        GM_xmlhttpRequest
+// @connect      open.oapi.vn
+// @icon         https://i.imgur.com/AM2f24N.png
 // ==/UserScript==
-
-// (Nội dung script chính)
-...
